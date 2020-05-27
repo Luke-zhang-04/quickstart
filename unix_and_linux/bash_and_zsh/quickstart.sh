62852d9f69f1ebaec7659bf2c1630898695cce70
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-05-27 18:15:16 -0400

    feat: initialized quickstart for bash and zsh
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ unix_and_linux/bash_and_zsh/quickstart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+args="$*"
+
+checkParams() {
+    echo "$args" "$2" "$3"
+    if [[ $args == *"$2"* ]]; then
+        eval $1=true
+    elif [[ $args == *"$3"* ]]&&[["$3"]]; then
+        eval $1=true
+    else 
+        eval $1=false
+    fi
+}
+
+checkParams react_app react
+checkParams typescript typescript ts
+checkParams eslint eslint
+checkParams bootstrap bootstrap bs
+
+echo "$react_app" "$typescript" "$eslint" "$bootstrap"
