81374e48373be3222221c2e4e97564866dc31b47
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-06 15:25:32 -0400

    initial commit
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Shell scripts to help you configure a project faster.
 ## Usage
 ### Example Create React App with Typescript and Eslint
 ```bash
-bash ./unix_and_linux/bash/main.sh ./example react ts eslint
+bash ./bash/main.sh react ts eslint
 ```
 ![Screen Shot 2020-05-27 at 9 16 14 PM](https://user-images.githubusercontent.com/55749227/83087961-85c7f480-a060-11ea-9ff7-d840dc026cf9.png)
--- bash/main.sh
+++ bash/main.sh
@@ -18,14 +18,42 @@ checkParams typescript typescript ts
 checkParams eslint eslint
 checkParams bootstrap bootstrap bs
 
+#######################################
+# Create react app
+# Globals:
+#   react_app
+#   typescript
+# Arguments:
+#   None
+#######################################
+reactApp() {
+    if [ "$react_app" ]&&[ "$typescript" ]; then # React app w/ typescript
+        # npx create-react-app client --template typescript # Create React app named client, with typescript template
+        # cd client # cd into client
+        echo "WOAH! REACT WITH TYPESCRIPT"
+    elif [ "$react_app" ]; then # React app wo/ typescript
+        # npx create-react-app client # Create React app names client
+        # cd client # cd into client
+        echo "WOAH! REACT WITH NO TYPESCRIPT"
+    fi
+}
+
+
+
 # Function which executes the commands
 quickStart() {
-    echo "first $1"
-    if [ "$react_app" ]&&[ "$typescript" ]; then # React app w/ typescript
-        npx create-react-app "$1" --template typescript
-    elif [ "$react_app" ]; then # React app wo/ typescript
-        npx create-react-app "$1" 
-    fi
+    # Clone the quickstart Gist. Check this Gist for malicious code first, in case you don't trust me (don't trust anyone).
+    git clone https://gist.github.com/d4c19d39f0a462fc79e0b3361752cf95.git
+    mv d4c19d39f0a462fc79e0b3361752cf95 quickstart # Change name to quickstart
+
+    mv ./quickstart/.gitignore ./.gitignore # Get .gitignore from quickstart
+
+    reactApp
+
+    if [ "$bootstrap" ]; then # If Bootstrap was requested
+        yarn add bootstrap
+
+    rm -rf ./quickstart # Get rid of quickstart
 }
 
 # Ask for proceeding
@@ -34,7 +62,8 @@ read -n 1 -p "Proceed? [Y/n] " proceed
 
 if [ "$proceed" = "Y" ]; then
     printf "\nProceeding with quickstart\n"
-    quickStart $1
+    quickStart
+    exit 0
 elif [ "$proceed" = "n" ]; then
     printf "\nAborting quickstart . . .\n"
     exit 0
