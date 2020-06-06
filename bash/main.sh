86c94d98ae2bd97345029da33e66c934615e38db
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-06 16:05:41 -0400

    feat(bash): add bootstrap option
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- bash/main.sh
+++ bash/main.sh
@@ -2,10 +2,19 @@
 
 args="$*" # Passed in arguments
 
+#######################################
+# Checks parameters
+# Globals:
+#   none
+# Arguments:
+#   global to set to
+#   full arg name
+#   short arg name (optional)
+#######################################
 checkParams() { # Check passed in arguments and match them
-    if [[ $args == *"$2"* ]]; then
+    if [[ "$args" == *"$2"* ]]; then
         eval "$1"=true
-    elif [[ $args == *"$3"* ]]&&[[ "$3" ]]; then
+    elif [[ "$args" == *"$3"* ]]&&[[ "$3" ]]; then
         eval "$1"=true
     else 
         eval "$1"=false
@@ -17,6 +26,7 @@ checkParams react_app react
 checkParams typescript typescript ts
 checkParams eslint eslint
 checkParams bootstrap bootstrap bs
+checkParams noVer noVer nv
 
 #######################################
 # Create react app
@@ -38,9 +48,33 @@ reactApp() {
     fi
 }
 
+#######################################
+# Create react app
+# Globals:
+#   bootstrap
+# Arguments:
+#   None
+#######################################
+getBootstrap() {
+    if "$bootstrap"; then # If Bootstrap was requested
+        yarn add bootstrap # Add bootstrap with Yarn
+        mkdir scss # Make a SCSS directory
 
+        # Add default imports into ./scss/bootstrap.scss
+        printf "/*!\n * Bootstrap v4.5.0 (https://getbootstrap.com/)\n * Copyright 2011-2020 The Bootstrap Authors\n * Copyright 2011-2020 Twitter, Inc.\n * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)\n */\n\n// Required\n@import \"./../node_modules/bootstrap/scss/functions\";\n@import \"./../node_modules/bootstrap/scss/variables\";\n@import \"./../node_modules/bootstrap/scss/mixins\";\n\n// Optional\n@import \"./../node_modules/bootstrap/scss/root\";\n@import \"./../node_modules/bootstrap/scss/reboot\";\n@import \"./../node_modules/bootstrap/scss/type\";\n@import \"./../node_modules/bootstrap/scss/images\";\n@import \"./../node_modules/bootstrap/scss/code\";\n@import \"./../node_modules/bootstrap/scss/grid\";\n@import \"./../node_modules/bootstrap/scss/tables\";\n@import \"./../node_modules/bootstrap/scss/forms\";\n@import \"./../node_modules/bootstrap/scss/buttons\";\n@import \"./../node_modules/bootstrap/scss/transitions\";\n@import \"./../node_modules/bootstrap/scss/dropdown\";\n@import \"./../node_modules/bootstrap/scss/button-group\";\n@import \"./../node_modules/bootstrap/scss/input-group\";\n@import \"./../node_modules/bootstrap/scss/custom-forms\";\n@import \"./../node_modules/bootstrap/scss/nav\";\n@import \"./../node_modules/bootstrap/scss/navbar\";\n@import \"./../node_modules/bootstrap/scss/card\";\n@import \"./../node_modules/bootstrap/scss/breadcrumb\";\n@import \"./../node_modules/bootstrap/scss/pagination\";\n@import \"./../node_modules/bootstrap/scss/badge\";\n@import \"./../node_modules/bootstrap/scss/jumbotron\";\n@import \"./../node_modules/bootstrap/scss/alert\";\n@import \"./../node_modules/bootstrap/scss/progress\";\n@import \"./../node_modules/bootstrap/scss/media\";\n@import \"./../node_modules/bootstrap/scss/list-group\";\n@import \"./../node_modules/bootstrap/scss/close\";\n@import \"./../node_modules/bootstrap/scss/toasts\";\n@import \"./../node_modules/bootstrap/scss/modal\";\n@import \"./../node_modules/bootstrap/scss/tooltip\";\n@import \"./../node_modules/bootstrap/scss/popover\";\n@import \"./../node_modules/bootstrap/scss/carousel\";\n@import \"./../node_modules/bootstrap/scss/spinners\";\n@import \"./../node_modules/bootstrap/scss/utilities\";\n@import \"./../node_modules/bootstrap/scss/print\";\n" >> scss/bootstrap.scss
+    fi
+}
 
-# Function which executes the commands
+#######################################
+# Main function
+# Globals:
+#   react_app
+#   typescript
+#   eslint
+#   bootstrap
+# Arguments:
+#   None
+#######################################
 quickStart() {
     # Clone the quickstart Gist. Check this Gist for malicious code first, in case you don't trust me (don't trust anyone).
     git clone https://gist.github.com/d4c19d39f0a462fc79e0b3361752cf95.git
@@ -50,16 +84,19 @@ quickStart() {
 
     reactApp
 
-    if [ "$bootstrap" ]; then # If Bootstrap was requested
-        yarn add bootstrap
-    fi
+    getBootstrap
 
     rm -rf ./quickstart # Get rid of quickstart
 }
 
 # Ask for proceeding
 printf "Preparing to quickstart with: $args\n"
-read -n 1 -p "Proceed? [Y/n] " proceed
+
+if ! [ "$noVer" ]; then
+    read -n 1 -p "Proceed? [Y/n] " proceed
+else
+    proceed="Y"
+fi
 
 if [ "$proceed" = "Y" ]; then
     printf "\nProceeding with quickstart\n"
