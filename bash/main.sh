18873f0c8765a9856fdaa55c7ef2f5fdb4160489
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-06 16:08:38 -0400

    feat(bash): add react app command

--- bash/main.sh
+++ bash/main.sh
@@ -28,24 +28,9 @@ checkParams eslint eslint
 checkParams bootstrap bootstrap bs
 checkParams noVer noVer nv
 
-#######################################
-# Create react app
-# Globals:
-#   react_app
-#   typescript
-# Arguments:
-#   None
-#######################################
-reactApp() {
-    if [ "$react_app" ]&&[ "$typescript" ]; then # React app w/ typescript
-        # npx create-react-app client --template typescript # Create React app named client, with typescript template
-        # cd client # cd into client
-        echo "WOAH! REACT WITH TYPESCRIPT"
-    elif [ "$react_app" ]; then # React app wo/ typescript
-        # npx create-react-app client # Create React app names client
-        # cd client # cd into client
-        echo "WOAH! REACT WITH NO TYPESCRIPT"
-    fi
+cleanup() {
+    cd .. # cd back 1 level
+    rm -rf ./quickstart # Get rid of quickstart
 }
 
 #######################################
@@ -62,6 +47,24 @@ getBootstrap() {
 
         # Add default imports into ./scss/bootstrap.scss
         printf "/*!\n * Bootstrap v4.5.0 (https://getbootstrap.com/)\n * Copyright 2011-2020 The Bootstrap Authors\n * Copyright 2011-2020 Twitter, Inc.\n * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)\n */\n\n// Required\n@import \"./../node_modules/bootstrap/scss/functions\";\n@import \"./../node_modules/bootstrap/scss/variables\";\n@import \"./../node_modules/bootstrap/scss/mixins\";\n\n// Optional\n@import \"./../node_modules/bootstrap/scss/root\";\n@import \"./../node_modules/bootstrap/scss/reboot\";\n@import \"./../node_modules/bootstrap/scss/type\";\n@import \"./../node_modules/bootstrap/scss/images\";\n@import \"./../node_modules/bootstrap/scss/code\";\n@import \"./../node_modules/bootstrap/scss/grid\";\n@import \"./../node_modules/bootstrap/scss/tables\";\n@import \"./../node_modules/bootstrap/scss/forms\";\n@import \"./../node_modules/bootstrap/scss/buttons\";\n@import \"./../node_modules/bootstrap/scss/transitions\";\n@import \"./../node_modules/bootstrap/scss/dropdown\";\n@import \"./../node_modules/bootstrap/scss/button-group\";\n@import \"./../node_modules/bootstrap/scss/input-group\";\n@import \"./../node_modules/bootstrap/scss/custom-forms\";\n@import \"./../node_modules/bootstrap/scss/nav\";\n@import \"./../node_modules/bootstrap/scss/navbar\";\n@import \"./../node_modules/bootstrap/scss/card\";\n@import \"./../node_modules/bootstrap/scss/breadcrumb\";\n@import \"./../node_modules/bootstrap/scss/pagination\";\n@import \"./../node_modules/bootstrap/scss/badge\";\n@import \"./../node_modules/bootstrap/scss/jumbotron\";\n@import \"./../node_modules/bootstrap/scss/alert\";\n@import \"./../node_modules/bootstrap/scss/progress\";\n@import \"./../node_modules/bootstrap/scss/media\";\n@import \"./../node_modules/bootstrap/scss/list-group\";\n@import \"./../node_modules/bootstrap/scss/close\";\n@import \"./../node_modules/bootstrap/scss/toasts\";\n@import \"./../node_modules/bootstrap/scss/modal\";\n@import \"./../node_modules/bootstrap/scss/tooltip\";\n@import \"./../node_modules/bootstrap/scss/popover\";\n@import \"./../node_modules/bootstrap/scss/carousel\";\n@import \"./../node_modules/bootstrap/scss/spinners\";\n@import \"./../node_modules/bootstrap/scss/utilities\";\n@import \"./../node_modules/bootstrap/scss/print\";\n" >> scss/bootstrap.scss
+    fi
+}
+
+#######################################
+# Create react app
+# Globals:
+#   react_app
+#   typescript
+# Arguments:
+#   None
+#######################################
+reactApp() {
+    if [ "$react_app" ]&&[ "$typescript" ]; then # React app w/ typescript
+        npx create-react-app client --template typescript # Create React app named client, with typescript template
+        cd client # cd into client
+    elif [ "$react_app" ]; then # React app wo/ typescript
+        npx create-react-app client # Create React app names client
+        cd client # cd into client
     fi
 }
 
@@ -86,7 +89,7 @@ quickStart() {
 
     getBootstrap
 
-    rm -rf ./quickstart # Get rid of quickstart
+    cleanup
 }
 
 # Ask for proceeding
