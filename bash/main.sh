316e2a565bbc5508da9827a59996462b8b311cc3
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-28 12:03:31 -0400

    feat(bash): add help option, callable with `-h` or `--help`
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- bash/main.sh
+++ bash/main.sh
@@ -4,6 +4,8 @@
 
 version="1.1.0"
 
+tabs 4
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # Get location of this script
 scripts=("bootstrap" "eslint" "react" "codeclimate" "stylelint" "make" "typescript")
 
@@ -57,6 +59,7 @@ noVer="$(checkParams noVer nv)"
 typescript="$(checkParams typescript ts)"
 reactApp="$(checkParams react)"
 stylelint="$(checkParams stylelint slint)"
+help="$(checkParams -h --help)"
 
 #######################################
 # Main function
@@ -118,6 +121,23 @@ quickStart() {
 # Ask for proceeding
 printf "$quickstartAscii"
 printf "$startupText"
+
+if $help; then
+    printf "${Cyan}Usage: ${IBlue}Quickstart ${IGreen}[options]${Cyan}\n\n"
+
+    printf "${IBlue}Options:${Cyan}\n"
+    printf "\t${IGreen}bootstrap bs${Cyan}    add bootstrap and configure SCSS file\n"
+    printf "\t${IGreen}codeclimate cc${Cyan}  add codeclimate configuration\n"
+    printf "\t${IGreen}eslint esl${Cyan}      add eslint configuration and ignore file\n"
+    printf "\t${IGreen}makefile make${Cyan}   add makefile with compile and lint commands\n"
+    printf "\t${IGreen}noVer nv${Cyan}        skip verification and download immediately\n"
+    printf "\t${IGreen}react${Cyan}           initialize React application\n"
+    printf "\t${IGreen}stylelint slint${Cyan} add stylelint configuration\n"
+    printf "\t${IGreen}typescript ts${Cyan}   add Typescript and configure as needed\n"
+    printf "\t${IGreen}-h --help${Cyan}       display help for command\n"
+    exit 0
+fi
+
 printf "${Cyan}Preparing to quickstart with:${Purple} $argString${RESET}\n"
 
 if $noVer; then
